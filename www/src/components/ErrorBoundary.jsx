eb8ce5467927799050dc2dbeb330921eed2424ed
Tilman Roeder <dyed.green.info@gmail.com>
2021-05-28 10:36:18 +0100

    Much better error names

--- www/src/components/ErrorBoundary.jsx
+++ www/src/components/ErrorBoundary.jsx
@@ -5,13 +5,21 @@ function reload() {
   window.location.href = window.location.href;
 }
 
+function errorToString(err) {
+  const obj = {};
+  for (const key of Object.getOwnPropertyNames(err)) {
+    obj[key] = err[key];
+  }
+  return JSON.stringify(obj);
+};
+
 function ErrorInfo({ error }) {
   return <div class="w-full flex justify-center p-8">
     <div class="w-full max-w-3xl bg-red-500 rounded-lg p-4">
       <h1 class="text-white text-xl font-semibold">Error</h1>
       <p class="text-red-100">Sorry, something went wrong.</p>
-      <pre class="text-white bg-red-900 rounded-lg p-4 mt-2 whitespace-pre-wrap">
-        {JSON.stringify(error)}
+      <pre class="text-white bg-red-900 rounded-lg p-4 mt-2 whitespace-pre-wrap break-all">
+        {errorToString(error)}
       </pre>
 
       <Button title="Reload Page" onClick={reload} type="flat" style="w-full mt-2" />
