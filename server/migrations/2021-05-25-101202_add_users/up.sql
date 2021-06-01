ecd24521a6d9b7adfd02b876080b2af23ac08f7e
Tilman Roeder <dyed.green.info@gmail.com>
2021-06-01 20:24:30 +0100

    Fix foreign key pragma for SQLite >:(

--- server/migrations/2021-05-25-101202_add_users/up.sql
+++ server/migrations/2021-05-25-101202_add_users/up.sql
@@ -1,5 +1,3 @@
-PRAGMA foreign_keys = ON;
-
 CREATE TABLE users (
   id          VARCHAR(21) PRIMARY KEY NOT NULL,
   created_at  TIMESTAMP NOT NULL,
--- server/src/db/mod.rs
+++ server/src/db/mod.rs
@@ -1,7 +1,8 @@
 use crate::config::Config;
 use anyhow::Result;
-use bb8_diesel::{bb8, DieselConnectionManager};
-use diesel::sqlite::SqliteConnection;
+use async_trait::async_trait;
+use bb8_diesel::{bb8, DieselConnection, DieselConnectionManager};
+use diesel::{sqlite::SqliteConnection, RunQueryDsl};
 
 pub mod id;
 pub mod models;
@@ -12,9 +13,33 @@ pub type PooledConnection<'a> =
 
 diesel_migrations::embed_migrations!();
 
+#[derive(Debug)]
+struct Customizer;
+
+#[async_trait]
+impl bb8::CustomizeConnection<DieselConnection<SqliteConnection>, diesel::r2d2::Error>
+    for Customizer
+{
+    async fn on_acquire(
+        &self,
+        conn: &mut DieselConnection<SqliteConnection>,
+    ) -> Result<(), diesel::r2d2::Error> {
+        let query = diesel::sql_query("PRAGMA foreign_keys = ON");
+        query.execute(&*conn).map_err(|err| {
+            log::error!("Failed to customize connection: {}", err);
+            diesel::r2d2::Error::QueryError(err)
+        })?;
+        Ok(())
+    }
+}
+
 pub async fn connect(config: &Config) -> Result<Pool> {
     let manager = DieselConnectionManager::new(config.database());
-    let pool = bb8::Pool::builder().max_size(8).build(manager).await?;
+    let pool = bb8::Pool::builder()
+        .max_size(8)
+        .connection_customizer(Box::new(Customizer))
+        .build(manager)
+        .await?;
 
     {
         // Run migrations
