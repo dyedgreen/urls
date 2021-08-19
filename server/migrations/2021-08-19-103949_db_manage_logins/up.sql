672fbeb6eeed724c56ef94b60a4f7fde8112a41b
Tilman Roeder <dyed.green.info@gmail.com>
2021-08-19 13:47:56 +0100

    Fix test error from migration

--- server/migrations/2021-08-19-103949_db_manage_logins/up.sql
+++ server/migrations/2021-08-19-103949_db_manage_logins/up.sql
@@ -1,2 +1,16 @@
-ALTER TABLE logins ADD COLUMN last_user_agent TEXT;
-ALTER TABLE logins ADD COLUMN revoked BOOLEAN NOT NULL;
+DROP TABLE logins;
+
+CREATE TABLE logins (
+  id          VARCHAR(21) PRIMARY KEY NOT NULL,
+  created_at  TIMESTAMP NOT NULL,
+  updated_at  TIMESTAMP NOT NULL,
+
+  user_id         VARCHAR(21) NOT NULL REFERENCES users(id),
+  email_token     TEXT NOT NULL,
+  claim_until     TIMESTAMP NOT NULL,
+  claimed         BOOLEAN NOT NULL,
+  session_token   TEXT UNIQUE,
+  last_used       TIMESTAMP NOT NULL,
+  last_user_agent TEXT,
+  revoked         BOOLEAN NOT NULL
+);
